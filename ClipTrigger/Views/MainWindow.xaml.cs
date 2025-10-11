46a2d9f2641dade95f4a391ad2e71d4b0ec38526
UmsrAkne <sibata486@gmail.com>
2025-10-11 22:20:04 +0900

    init

---
+++ ClipTrigger/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ClipTrigger.Views;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
