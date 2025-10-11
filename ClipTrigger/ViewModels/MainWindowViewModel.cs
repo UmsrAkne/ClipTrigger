b063ee8fe5cb696137db0be3acee4c2f37c1cdd0
UmsrAkne <sibata486@gmail.com>
2025-10-11 22:49:45 +0900

    feat(ui): enhance MainWindow with source directory input and display functionality

--- ClipTrigger/ViewModels/MainWindowViewModel.cs
+++ ClipTrigger/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,33 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using Prism.Mvvm;
 
 namespace ClipTrigger.ViewModels;
 
 public class MainWindowViewModel : BindableBase
 {
-    private string _title = "Prism Application";
+    private string title = "Prism Application";
+
+    public MainWindowViewModel()
+    {
+        InjectDummies();
+    }
 
     public string Title
     {
-        get => _title;
-        set => SetProperty(ref _title, value);
+        get => title;
+        set => SetProperty(ref title, value);
+    }
+
+    public ObservableCollection<DirectoryInfo> SourceDirectories { get; set; } = new ();
+
+    private void InjectDummies()
+    {
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test"));
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test1"));
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test2"));
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test3"));
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test4"));
+        SourceDirectories.Add(new DirectoryInfo(@"C:\Users\testUser\Desktop\test5"));
     }
 }
